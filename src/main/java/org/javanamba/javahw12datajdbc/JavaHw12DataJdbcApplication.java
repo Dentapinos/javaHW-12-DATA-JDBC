f584f7307624c1f29214a00cb3dc074298fb1e02
DenisLadchenkov <119222041+Dentapinos@users.noreply.github.com>
2025-08-10 21:21:49 +0300

    init

---
+++ src/main/java/org/javanamba/javahw12datajdbc/JavaHw12DataJdbcApplication.java
@@ -0,0 +1,13 @@
+package org.javanamba.javahw12datajdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHw12DataJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHw12DataJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javanamba/javahw12datajdbc/JavaHw12DataJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javanamba.javahw12datajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHw12DataJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
